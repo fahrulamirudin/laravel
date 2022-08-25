4ad4ab9fb4d0f17ff0f492ac563c67b5980b6f9e
fahrulamirudin <58175401+fahrulamirudin@users.noreply.github.com>
2022-08-25 16:15:19 +0700

    Install Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+//resources/js/app.js
+import Vue from 'vue';
+
+import App from './components/ExampleComponent.vue'
+
+
+new Vue({
+  el: '#app',
+  components: { App }
+});
